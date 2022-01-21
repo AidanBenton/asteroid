8d022cd0fccb4ab4d9e6e23eab4fb777b2d24bb2
Aidan <aidanbenton2005@gmail.com>
2022-01-21 14:53:39 -0500

    Finished up audio as well as polished a couple of things up

--- Space race/Form1.Designer.cs
+++ Space race/Form1.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.components = new System.ComponentModel.Container();
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.textLabel = new System.Windows.Forms.Label();
+            this.playerOneLabel = new System.Windows.Forms.Label();
+            this.playerTwoLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // gameTimer
@@ -38,12 +41,51 @@
             this.gameTimer.Interval = 17;
             this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
             // 
+            // textLabel
+            // 
+            this.textLabel.BackColor = System.Drawing.Color.Transparent;
+            this.textLabel.Font = new System.Drawing.Font("Consolas", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textLabel.Location = new System.Drawing.Point(174, -1);
+            this.textLabel.Name = "textLabel";
+            this.textLabel.Size = new System.Drawing.Size(239, 285);
+            this.textLabel.TabIndex = 1;
+            this.textLabel.Text = "label1";
+            this.textLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // playerOneLabel
+            // 
+            this.playerOneLabel.BackColor = System.Drawing.Color.Transparent;
+            this.playerOneLabel.Font = new System.Drawing.Font("Consolas", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerOneLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.playerOneLabel.Location = new System.Drawing.Point(12, 226);
+            this.playerOneLabel.Name = "playerOneLabel";
+            this.playerOneLabel.Size = new System.Drawing.Size(174, 198);
+            this.playerOneLabel.TabIndex = 2;
+            this.playerOneLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // playerTwoLabel
+            // 
+            this.playerTwoLabel.BackColor = System.Drawing.Color.Transparent;
+            this.playerTwoLabel.Font = new System.Drawing.Font("Consolas", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerTwoLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.playerTwoLabel.Location = new System.Drawing.Point(424, 226);
+            this.playerTwoLabel.Name = "playerTwoLabel";
+            this.playerTwoLabel.Size = new System.Drawing.Size(174, 198);
+            this.playerTwoLabel.TabIndex = 3;
+            this.playerTwoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(634, 361);
+            this.ClientSize = new System.Drawing.Size(610, 360);
+            this.Controls.Add(this.playerTwoLabel);
+            this.Controls.Add(this.playerOneLabel);
+            this.Controls.Add(this.textLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
@@ -56,6 +98,9 @@
         #endregion
 
         private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label textLabel;
+        private System.Windows.Forms.Label playerOneLabel;
+        private System.Windows.Forms.Label playerTwoLabel;
     }
 }
 
--- Space race/Form1.cs
+++ Space race/Form1.cs
@@ -7,24 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Media;
+using System.IO;
+//please be carfull with the audio I wasnt able to find a solutuin to change the volume levels of the sounds so some of them are 
+//louded then I would have liked them to, So just be carfull 
+//in addition there was a startup sound that I wasnt able fix although it does kind of fit
 namespace Space_race
 {
     public partial class Form1 : Form
     {
+        //input variables
         bool wDown;
         bool sDown;
-        bool aDown;
-        bool dDown;
+        bool enter;
+        bool Esc;
 
         bool upArrowDown;
         bool downArrowDown;
-        bool leftArrowDown;
-        bool rightArrowDown;
 
+        //player stats
         int playerspeed = 5;
-        int playersize = 10;
 
+        int player1score = 0;
+        int player2score = 0;
+
+        //used to start game
+        string gamestate = "title";
+
+        //lists for astroid data storage 
         List<Rectangle> leftAstroids = new List<Rectangle>();
         List<int> leftAstroidsSpeeds = new List<int>();
 
@@ -35,22 +45,42 @@ namespace Space_race
 
         bool check = false;
 
+        //used for random astroid creation 
         Random randGen = new Random();
-        int randValue = 0; 
+        int randValue = 0;
 
+        //used for painting screen 
         SolidBrush whiteBrush = new SolidBrush(Color.White);
 
-        Rectangle player1 = new Rectangle(143, 340, 10, 10);
-        Rectangle player2 = new Rectangle(427, 340, 10, 10); 
+        //player rectangles 
+        Rectangle player1 = new Rectangle(143, 345, 10, 10);
+        Rectangle player2 = new Rectangle(452, 345, 10, 10);
+
+        //sound media players
+        System.Windows.Media.MediaPlayer menuSound = new System.Windows.Media.MediaPlayer();
+        System.Windows.Media.MediaPlayer background = new System.Windows.Media.MediaPlayer();
+        System.Windows.Media.MediaPlayer scoreSound = new System.Windows.Media.MediaPlayer();
+        System.Windows.Media.MediaPlayer gameOver = new System.Windows.Media.MediaPlayer();
+
 
         public Form1()
         {
             InitializeComponent();
+
+            //needed for sound media players to function
+            menuSound.Open(new Uri(Application.StartupPath + "/Resources/Clink.wav"));
+
+            scoreSound.Open(new Uri(Application.StartupPath + "/Resources/Score.wav"));
+
+            gameOver.Open(new Uri(Application.StartupPath + "/Resources/gameOver.wav"));
+
+            background.Open(new Uri(Application.StartupPath + "/Resources/Background.wav"));
+            background.MediaEnded += new EventHandler(background_MediaEnded);
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-
+            // player inputs 
             switch (e.KeyCode)
             {
                 case Keys.W:
@@ -59,31 +89,24 @@ namespace Space_race
                 case Keys.S:
                     sDown = true;
                     break;
-                case Keys.A:
-                    aDown = true;
-                    break;
-                case Keys.D:
-                    dDown = true;
-                    break;
                 case Keys.Up:
                     upArrowDown = true;
                     break;
                 case Keys.Down:
                     downArrowDown = true;
                     break;
-                case Keys.Left:
-                    leftArrowDown = true;
+                case Keys.Enter:
+                    enter = true;
                     break;
-                case Keys.Right:
-                    rightArrowDown = true;
+                case Keys.Escape:
+                    Esc = true;
                     break;
             }
-
         }
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
-
+            //player inputs 
             switch (e.KeyCode)
             {
                 case Keys.W:
@@ -92,73 +115,31 @@ namespace Space_race
                 case Keys.S:
                     sDown = false;
                     break;
-                case Keys.A:
-                    aDown = false;
-                    break;
-                case Keys.D:
-                    dDown = false;
-                    break;
                 case Keys.Up:
                     upArrowDown = false;
                     break;
                 case Keys.Down:
                     downArrowDown = false;
                     break;
-                case Keys.Left:
-                    leftArrowDown = false;
+                case Keys.Enter:
+                    enter = false;
                     break;
-                case Keys.Right:
-                    rightArrowDown = false;
+                case Keys.Escape:
+                    Esc = false;
                     break;
             }
 
         }
         private void gameTimer_Tick(object sender, EventArgs e)
         {
-            if (wDown == true && player1.Y >= 0)
-            {
-                player1.Y -= playerspeed;
-            }
-            //was having trouble with the plus value as it doesnt make sence as my rectangle width
-            //is only 20 pixels so im not sure why this was happening but I just guessed and checked
-            if (sDown == true && player1.Y < this.Height - player1.Height)
-            {
-                player1.Y += playerspeed;
-            }
-            if (aDown == true && player1.X >= 0)
-            {
-                player1.X -= playerspeed;
-            }
-            if (dDown == true && player1.X <this.Width - player1.Width)
-            {
-                player1.X += playerspeed;
-            }
-
-            if (upArrowDown == true && player2.Y >= 0)
-            {
-                player2.Y -= playerspeed;
-            }
-            //was having trouble with the plus value as it doesnt make sence as my rectangle width
-            //is only 20 pixels so im not sure why this was happening but I just guessed and checked
-            if (downArrowDown == true && player2.Y + 65 <= this.Height)
-            {
-                player2.Y += playerspeed;
-            }
-            if (leftArrowDown == true && player2.X >= 0)
-            {
-                player2.X -= playerspeed;
-            }
-            if (rightArrowDown == true && player2.X + 40 <= this.Width)
-            {
-                player2.X += playerspeed;
-            }
-
+            //I know its inefficiant to have the astroids running during times they arnt needed but this way when the player
+            //starts the game the astoid feild is already full
             //moves astroids
             for (int i = 0; i < leftAstroids.Count(); i++)
             {
                 //usings astroidspeed variable to make the astroids move at differant speeds
                 int x = leftAstroids[i].X + leftAstroidsSpeeds[i];
-                leftAstroids[i] = new Rectangle(x, leftAstroids[i].Y, 10, 5); 
+                leftAstroids[i] = new Rectangle(x, leftAstroids[i].Y, 10, 5);
             }
 
             for (int i = 0; i < rightAstroids.Count(); i++)
@@ -169,38 +150,18 @@ namespace Space_race
             }
 
             //creates new astroids 
-            randValue = randGen.Next(0, 101); 
+            randValue = randGen.Next(0, 101);
 
-            if(randValue < 20)
+            if (randValue < 10)
             {
-                int y = randGen.Next(20, this.Height - 20);
-                for (int i = 0; i < leftAstroids.Count && check == false; i++)
-                {  
-                    for (int j = 0; j < twentyCheck.Count; j++)
-                    {
-                        if (y == leftAstroids[i].X + j && y == rightAstroids[i].X + j)
-                        {
-                           y = randGen.Next(20, this.Height - 20);
-                        }
-                    }
-                }
-                leftAstroids.Add(new Rectangle(-20 , y, 20, 20));
-                leftAstroidsSpeeds.Add(randGen.Next(2, 6)); 
+                int y = randGen.Next(20, this.Height - 50);
+                leftAstroids.Add(new Rectangle(-20, y, 20, 20));
+                leftAstroidsSpeeds.Add(randGen.Next(2, 6));
             }
 
-            if (randValue > 20 && randValue < 40)
+            if (randValue > 20 && randValue < 30)
             {
-                int y = randGen.Next(20, this.Height - 20);
-                for (int i = 0; i < rightAstroids.Count && check == false; i++)
-                {
-                    for (int j = 0; j < twentyCheck.Count; j++)
-                    {
-                        if (y == leftAstroids[i].X + j && y == rightAstroids[i].X + j)
-                        {
-                            y = randGen.Next(20, this.Height - 20);
-                        }
-                    }
-                }
+                int y = randGen.Next(20, this.Height - 50);
                 rightAstroids.Add(new Rectangle(this.Width, y, 20, 20));
                 rightAstroidsSpeeds.Add(randGen.Next(2, 6));
             }
@@ -211,7 +172,9 @@ namespace Space_race
                 if (leftAstroids[i].X + 20 > this.Width)
                 {
                     leftAstroids.RemoveAt(i);
-                    leftAstroidsSpeeds.Remove(i);
+                    leftAstroidsSpeeds.RemoveAt(i);
+                    break;
+
                 }
 
             }
@@ -222,27 +185,218 @@ namespace Space_race
                 if (rightAstroids[i].X + 10 < 0)
                 {
                     rightAstroids.RemoveAt(i);
-                    rightAstroidsSpeeds.Remove(i);
+                    rightAstroidsSpeeds.RemoveAt(i);
+                    break;
                 }
 
+            }
+
+            //checks the specific gamestate that is running
+            if (gamestate == "title")
+            {
+                if (enter == true)
+                {
+                    gamestate = "running";
+                    textLabel.Text = "";
+                    menuSound.Stop();
+                    menuSound.Play();
+                    background.Play();
+                }
+
+                if (Esc == true)
+                {
+                    menuSound.Stop();
+                    menuSound.Play();
+                    this.Close();
+                }
+            }
+
+            //game state check 
+            if (gamestate == "running")
+            {
+                if (wDown == true)
+                {
+                    player1.Y -= playerspeed;
+                }
+                //was having trouble with the plus value as it doesnt make sence as my rectangle width
+                //is only 20 pixels so im not sure why this was happening but I just guessed and checked
+                if (sDown == true && player1.Y < this.Height - player1.Height - 5)
+                {
+                    player1.Y += playerspeed;
+                }
+
+                if (upArrowDown == true)
+                {
+                    player2.Y -= playerspeed;
+                }
+                //was having trouble with the plus value as it doesnt make sence as my rectangle width
+                //is only 20 pixels so im not sure why this was happening but I just guessed and checked
+                if (downArrowDown == true && player2.Y < this.Height - player2.Height - 5)
+                {
+                    player2.Y += playerspeed;
+                }
+
+
+                //Checks for player intercection with astroids 
+                for (int i = 0; i < leftAstroids.Count; ++i)
+                {
+                    if (player1.IntersectsWith(leftAstroids[i]))
+                    {
+                        gameOver.Stop();
+                        gameOver.Play();
+                        player1.Y = 340;
+                    }
+
+                    if (player2.IntersectsWith(leftAstroids[i]))
+                    {
+                        gameOver.Stop();
+                        gameOver.Play();
+                        player2.Y = 340;
+                    }
+                }
+
+                for (int i = 0; i < rightAstroids.Count; ++i)
+                {
+                    if (player1.IntersectsWith(rightAstroids[i]))
+                    {
+                        gameOver.Stop();
+                        gameOver.Play();
+                        player1.Y = 340;
+                    }
+
+                    if (player2.IntersectsWith(rightAstroids[i]))
+                    {
+                        gameOver.Stop();
+                        gameOver.Play();
+                        player2.Y = 340;
+                    }
+                }
+                //player scoreing 
+                if (player1.Y < 0)
+                {
+                    player1.Y = 340;
+                    player1score++;
+                    scoreSound.Stop();
+                    scoreSound.Play();
+                }
+
+                if (player2.Y < 0)
+                {
+                    player2.Y = 340;
+                    player2score++;
+                    scoreSound.Stop();
+                    scoreSound.Play();
+                }
+
+                //player score being displayed
+                if (player1score > 0)
+                {
+                    playerOneLabel.Text = $"{player1score}";
+                }
+
+                if (player2score > 0)
+                {
+                    playerTwoLabel.Text = $"{player2score}";
+                }
+
+                //changing game state
+                if (player1score == 3)
+                {
+                    background.Stop();
+                    gamestate = "End";
+                    Refresh();
+
+                }
+                if (player2score == 3)
+                {
+                    background.Stop();
+                    gamestate = "End";
+                    Refresh();
+
+                }
+            }
+
+            //The end game state
+            if (gamestate == "End")
+            {
+                if (enter == true)
+                {
+                    menuSound.Stop();
+                    menuSound.Play();
+                    background.Play();
+                    gamestate = "running";
+                    textLabel.Text = "";
+                    player1score = 0;
+                    player2score = 0;
+                    Refresh();
+                }
+                if (Esc == true)
+                {
+                    menuSound.Stop();
+                    menuSound.Play();
+                    this.Close();
+                }
             }
             Refresh();
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            e.Graphics.FillRectangle(whiteBrush, player1);
-            e.Graphics.FillRectangle(whiteBrush, player2);
-            
-            for (int i = 0; i < leftAstroids.Count; i++)
+            //graphics used for the boarder
+            e.Graphics.FillRectangle(whiteBrush, 0, 0, this.Width, 5);
+            e.Graphics.FillRectangle(whiteBrush, 0, 0, 5, this.Height);
+            e.Graphics.FillRectangle(whiteBrush, this.Width - 5, 0, 5, this.Height);
+            e.Graphics.FillRectangle(whiteBrush, 0, this.Height - 5, this.Width, 5);
+
+            //Gamestate chek 
+            if (gamestate == "title")
             {
-                e.Graphics.FillRectangle(whiteBrush, leftAstroids[i]); 
+                textLabel.Text = $"SPACE RACE\n\n\nEnter: Play\nEsc: Exit";
             }
 
-            for (int i = 0; i < rightAstroids.Count; i++)
+            else if (gamestate == "running")
             {
-                e.Graphics.FillRectangle(whiteBrush, rightAstroids[i]);
+                //paints players and astroids 
+                e.Graphics.FillRectangle(whiteBrush, player1);
+                e.Graphics.FillRectangle(whiteBrush, player2);
+                e.Graphics.FillRectangle(whiteBrush, 295, 0, 5, 400);
+                for (int i = 0; i < leftAstroids.Count; i++)
+                {
+                    e.Graphics.FillRectangle(whiteBrush, leftAstroids[i]);
+                }
+
+                for (int i = 0; i < rightAstroids.Count; i++)
+                {
+                    e.Graphics.FillRectangle(whiteBrush, rightAstroids[i]);
+                }
+
             }
+
+            //game has ended 
+            else
+            {
+                if (player1score == 3)
+                {
+                    textLabel.Text = $"PLAYER ONE WINS\n\nEnter: Play\nEsc: Exit";
+                }
+
+                if (player2score == 3)
+                {
+                    textLabel.Text = $"PLAYER TWO WINS\n\nEnter: Play\nEsc: Exit";
+                }
+
+                playerOneLabel.Text = "";
+                playerTwoLabel.Text = "";
+            }
+        }
+
+
+        //used for looping back sound
+        private void background_MediaEnded(object sender, EventArgs e)
+
+        {
+            background.Stop();
+            background.Play();
         }
     }
 }
